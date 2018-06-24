4d41d4cb041251e74f796963c9cf1cdcd7e14fde
enningxie <enningxie@163.com>
2018-06-24 21:33:01 +0800

    x

--- check_in.sh
+++ check_in.sh
@@ -1,5 +1,6 @@
 HOME=/home/enningxie &&
 date >> /home/enningxie/Codes/check_in/time.log &&
+nvidia-smi >> /home/enningxie/Codes/check_in/gpu.log &&
 cd /home/enningxie/Codes/check_in &&
 /usr/bin/git add . &&
 /usr/bin/git commit -m 'x' &&
