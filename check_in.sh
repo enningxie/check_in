88788aa44eaf5129d189221f898b90f93ba7581b
enningxie <enningxie@163.com>
2018-06-24 16:44:13 +0800

    x

---
+++ check_in.sh
@@ -0,0 +1,6 @@
+HOME=/home/enningxie &&
+date >> /home/enningxie/Codes/check_in/time.log &&
+cd /home/enningxie/Codes/check_in &&
+/usr/bin/git add . &&
+/usr/bin/git commit -m 'x' &&
+/usr/bin/git push origin master
